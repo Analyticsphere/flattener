1a8c364f54b248d64a237d214d9237d9f5fdf049
Eddy Frankenberger <edward.frankenberger@nih.gov>
2025-03-15 13:04:08 -0400

    add logging

--- core/flatten.py
+++ core/flatten.py
@@ -210,7 +210,9 @@ def create_flattening_select_statement(parque_path: str) -> str:
 
 
 def flatten_table(destination_bucket: str, project_id: str, dataset_id: str, table_name: str) -> None:
+    utils.logger.warning(f"flattening parquets in table {destination_bucket}")
     source_parquet_path = build_source_parquet_file_location(destination_bucket, table_name)
+    utils.logger.warning(f"looking for files ins {source_parquet_path}")
     select_statement = create_flattening_select_statement(source_parquet_path)
     select_no_return = select_statement.replace('\n', ' ')
     utils.logger.warning(f"\n**** Final select statement is **** \n {select_no_return}")
