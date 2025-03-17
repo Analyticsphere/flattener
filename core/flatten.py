e79fa2c5fc79c5939bf615533f6fa90f120f9d58
Eddy Frankenberger <edward.frankenberger@nih.gov>
2025-03-17 09:46:44 -0400

    fix select statement

--- core/flatten.py
+++ core/flatten.py
@@ -188,7 +188,7 @@ def create_flattening_select_statement(parque_path: str) -> str:
                 final_query = f"""
                     SELECT
                     {', '.join(select_exprs)}
-                    FROM read_parquet('{parque_path}')`
+                    FROM read_parquet('{parque_path}')
                 """
 
                 utils.logger.warning(f"\n\nFinal query generated with {len(select_exprs)} fields\n\n")
