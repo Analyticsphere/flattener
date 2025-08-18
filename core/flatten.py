861c7942309c172a8924e2c5ec12057238262531
cooknaa <nanci.cook@nih.gov>
2025-08-18 16:22:07 -0400

    Created new if/else statement.

--- core/flatten.py
+++ core/flatten.py
@@ -232,10 +232,16 @@ def create_flattening_select_statement(parquet_path: str) -> str:
                                 # Escape the value for SQL
                                 escaped_val = escape_sql_value(val)
                                 
+                                if escaped_val is "NULL":
+                                    expr = f"CAST({sql_path} AS STRING) AS STRING) AS \"{new_col_name}\""
+                                    select_exprs.append(expr)
+                                else: 
+                                    expr = f"CAST(CAST(array_contains({sql_path}, '{escaped_val}') AS INTEGER) AS STRING) AS \"{new_col_name}\"" #<-- Original Code
+                                    select_exprs.append(expr)
                                 # Create expression for binary indicator (1 if array contains value, 0 otherwise)
-                                expr = f"CAST(IFNULL(CAST(array_contains({sql_path}, '{escaped_val}') AS INTEGER), 0) AS STRING) AS \"{new_col_name}\"" #<-- Original Code
+                                # expr = f"CAST(IFNULL(CAST(array_contains({sql_path}, '{escaped_val}') AS INTEGER), 0) AS STRING) AS \"{new_col_name}\"" #<-- Original Code
                                 
-                                select_exprs.append(expr)
+                                # select_exprs.append(expr)
                                 
                                 
                         except Exception as e:
