1e7ceab520cb23f8b71b9d0c620e147f591ad822
cooknaa <nanci.cook@nih.gov>
2025-08-19 09:49:29 -0400

    Adjusted if/else statement.

--- core/flatten.py
+++ core/flatten.py
@@ -233,7 +233,7 @@ def create_flattening_select_statement(parquet_path: str) -> str:
                                 escaped_val = escape_sql_value(val)
                                 
                                 if escaped_val is 'NULL': 
-                                    expr = f"CAST({sql_path} AS STRING) AS \"{new_col_name}\""
+                                    expr = f"{sql_path} AS STRING AS \"{new_col_name}\""
                                     select_exprs.append(expr)
                                 else: 
                                     expr = f"CAST(CAST(array_contains({sql_path}, '{escaped_val}') AS INTEGER) AS STRING) AS \"{new_col_name}\"" #<-- Original Code
